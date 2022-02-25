bf8bdc6301543815aafa7748f2f66104e41c70c1
luoyangwei <qq123123>
2022-02-25 18:02:02 +0800

    init

---
+++ src/com/luoyangwei/web3j/demo/Web3jDemoMain.java
@@ -0,0 +1,12 @@
+package com.luoyangwei.web3j.demo;
+
+/**
+ * @author luoyangwei
+ */
+public class Web3jDemoMain {
+
+    public static void main(String[] args) {
+        // write your code here
+        // 一个Web3j的Demo项目
+    }
+}
